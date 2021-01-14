53853fb4ed57a79e64b47d583d6df3a96b3091ca
RM <rishabhmadan17@gmail.com>
2021-01-14 18:49:57 -0500

    Obfuscated the creds

--- api/src/config/config.js
+++ api/src/config/config.js
@@ -1,7 +1,7 @@
 const config = {
   AWS: {
-    access_key_id: "AKIA5I32XYA3MAKWD3XU",
-    secret_access_key: "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",
+    access_key_id: "AKIA5I32XYArtr3MAKWD3XU",
+    secret_access_key: "0s9Sv31CrtrXEFgD3uE56MbloI78ioiRTeuq52v5qYq",
     s3_bucket_name: "my-heart-media",
   },
 };
